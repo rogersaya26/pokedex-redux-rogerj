7bdcc7a79435a93bb82ec59488f6fd73df4819a2
rogersaya26 <rogersaya@gmail.com>
2022-05-05 10:27:22 -0300

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <h2>re</h2>
+      <img src="" alt="" />
+      <h2>te</h2>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+const Main = () => {
+
+  const [url, setUrl] = useState();
+
+
+
+  return (
+    <div>
+      Main
+    </div>
+  )
+}
+
+export default Main
